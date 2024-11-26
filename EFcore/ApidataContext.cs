9c8cadebf67a1595b25f67208853f8ffc1ab0564
hanson <sfhkzcbm02@gmail.com>
2024-11-26 21:55:28 +0800

    刪除敏感資料

--- EFcore/ApidataContext.cs
+++ EFcore/ApidataContext.cs
@@ -27,9 +27,7 @@ public partial class ApidataContext : DbContext
 
     public virtual DbSet<UserInfo> UserInfos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=apidata;User=sa;Password=!Zxc1010179;Trusted_Connection=True;TrustServerCertificate=true;");
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
